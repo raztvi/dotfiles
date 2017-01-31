554fafc7005bad579c134049ac245d4fd7b427b8
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-01-31 02:05:54 +0000

    Started working on scripts that will automate whole strapping process

---
+++ installScripts/brewStrap.sh
@@ -0,0 +1,49 @@
+if test ! $(which brew); then
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo -e "\n\nInstalling homebrew packages..."
+echo "=============================="
+
+formulas=(
+    # flags should pass through the the `brew list check`
+    'macvim --with-override-system-vim'
+    ack
+    aws-shell
+    diff-so-fancy
+    dnsmasq
+    fzf
+    git
+    'grep --with-default-names'
+    heroku
+    highlight
+    hub
+    jenkins
+    leiningen
+    markdown
+    mongodb
+    mysql
+    neovim/neovim/neovim
+    nginx
+    postgresql
+    reattach-to-user-namespace
+    rust
+    sbt
+    scala
+    the_silver_searcher
+    tmux
+    tree
+    wget
+    yarn
+    z
+    zsh
+)
+
+for formula in "${formulas[@]}"; do
+    if brew list "$formula" > /dev/null 2>&1; then
+        echo "$formula already installed... skipping."
+    else
+        brew install $formula
+    fi
+done
