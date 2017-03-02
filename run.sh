99014237ecc3c5ad95e5d360c8612facfd1ce1b3
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-02 19:36:58 +0000

    removed unecessary run.sh file

--- run.sh
+++ run.sh
@@ -1,17 +1,1 @@
-#!/bin/sh
-
-git filter-branch --env-filter '
-OLD_EMAIL="andrei.nicolae@avarix.co.uk"
-CORRECT_NAME="Andrei-Mihai Nicolae"
-CORRECT_EMAIL="a.mihai.nicolae@gmail.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='Andrei-Mihai Nicolae'; GIT_AUTHOR_EMAIL='a.mihai.nicolae@gmail.com'; GIT_COMMITTER_NAME='Andrei-Mihai Nicolae'; GIT_COMMITTER_EMAIL='a.mihai.nicolae@gmail.com';" HEAD
