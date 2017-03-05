5b47e5efe036c7ad9ab8638f583e76abfb34a47e
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-05 20:01:53 +0000

    added all apps that should be installed on a fresh mac

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -2,13 +2,44 @@ echo -e "\n\nInstalling homebrew casks..."
 echo "=============================="
 
 formulas=(
-    
+       google-chrome 
+       firefox
+       1password
+       skype
+       bartender
+       alfred
+       iterm2
+       atom
+       intellij-idea
+       pycharm
+       rubymine
+       sequel-pro
+       slack
+       whatsapp
+       webstorm
+       spotify
+       coconutbattery
+       github-desktop
+       sourcetree
+       caption
+       docker
+       hacker-menu
+       handbrake
+       postman
+       robomongo
+       spectacle
+       transmit
+       visual-studio-code
+       vlc
+       unetbootin
+       textmate
+       teamviewer
 )
 
 for formula in "${formulas[@]}"; do
     if brew list "$formula" > /dev/null 2>&1; then
         echo "$formula already installed... skipping."
     else
-        brew install $formula
+        brew cask install $formula
     fi
 done
