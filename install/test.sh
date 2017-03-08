65564eb633b647b057958130096fe52abccdcc2f
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-08 10:25:02 +0000

    removed unecessary test script

--- install/test.sh
+++
@@ -1,8 +0,0 @@
-# create an array with all the filer/dir inside ~/myDir
-arr=(/Applications/*)
-
-# iterate through array using a counter
-for ((i=0; i<${#arr[@]}; i++)); do
-    #do something to each element of array
-    echo "${arr[$i]}"
-done
